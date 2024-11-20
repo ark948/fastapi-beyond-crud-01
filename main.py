cf4fee5444dffc197bfa4f56f6b9a36d26b7fae0
ark948 <armankarimi948@gmail.com>
2024-11-20 16:08:43 +0330

    path parameter

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ app = FastAPI()
 @app.get('/')
 async def read_root():
     return {'message': "Hello World"}
+
+
+# path parameter
+@app.get('/greet/{name}')
+async def greet_name(name: str) -> dict:
+    return {"message": f'Hello {name}'}
+# http://127.0.0.1:8000/greet/john
