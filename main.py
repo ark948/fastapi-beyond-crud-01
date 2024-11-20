bf045d4393ffa0ccf76e97c7e27ad8b5f9877766
ark948 <armankarimi948@gmail.com>
2024-11-20 16:16:23 +0330

    path and query mixed

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ async def read_root():
     return {'message': "Hello World"}
 
 
+
 # path parameter
 @app.get('/greet/{name}')
 async def greet_name(name: str) -> dict:
@@ -26,3 +27,11 @@ async def greet_name(name: str) -> dict:
 async def greet_name(name: str) -> dict:
     return {"message": f'Hello {name}'}
 # http://127.0.0.1:8000/greet/?name=wick
+
+
+
+# path and query param mixed
+@app.get('/greet-v2/{name}')
+async def greet_name_v2(name: str, age: int) -> dict:
+    return {"message": f"hello {name}, age: {age}"}
+# http://127.0.0.1:8000/greet-v2/?name=trevor?age=29
