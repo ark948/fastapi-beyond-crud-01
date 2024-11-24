bbd99dc7f3241b09687f8a6c7c90f8a390ee04ef
ark948 <armankarimi948@gmail.com>
2024-11-24 15:04:28 +0330

    Added user book one to many relationship

--- src/books/models.py
+++ src/books/models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 from datetime import datetime, date
 import uuid
+from typing import Optional
 
 
 
@@ -21,6 +22,7 @@ class Book(SQLModel, table=True):
     published_date: date
     page_count: int
     language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
--- src/books/routes.py
+++ src/books/routes.py
@@ -20,23 +20,34 @@ async def get_all_books(
     user_data=Depends(access_token_bearer),
     _: bool = Depends(role_checker)
 ):
-    print(user_data)
     books = await book_service.get_all_books(session)
+    return books
+
+
+@book_router.get('/user/{user_uid}', response_model=List[Book])
+async def get_user_books_submissions(
+    user_uid: str,
+    session: AsyncSession = Depends(get_session),
+    user_data=Depends(access_token_bearer),
+    _: bool = Depends(role_checker)
+):
+    books = await book_service.get_user_books(user_uid, session)
     return books
 
 
 @book_router.post('/', status_code=status.HTTP_201_CREATED, response_model=Book)
 async def create_a_book(
-    book_data: BookCreateModel, session: AsyncSession = Depends(get_session),
-    user_data=Depends(access_token_bearer),
-    _: bool = Depends(role_checker)
-) -> dict:
-    new_book = await book_service.create_book(book_data, session)
+        book_data: BookCreateModel, session: AsyncSession = Depends(get_session),
+        token_details: dict=Depends(access_token_bearer),
+        _: bool = Depends(role_checker)
+    ) -> dict:
+    user_uid = token_details.get('user')['user_uid']
+    new_book = await book_service.create_book(book_data, user_uid, session)
     return new_book
 
 
 @book_router.get('/{book_uid}', response_model=Book)
-async def get_book(book_uid: str, session: AsyncSession = Depends(get_session), user_data=Depends(access_token_bearer), _: bool = Depends(role_checker)) -> dict:
+async def get_book(book_uid: str, session: AsyncSession = Depends(get_session), token_details: dict=Depends(access_token_bearer), _: bool = Depends(role_checker)) -> dict:
     book = await book_service.get_book(book_uid, session)
     if book:
         return book
@@ -46,7 +57,7 @@ async def get_book(book_uid: str, session: AsyncSession = Depends(get_session), 
 
 
 @book_router.patch('/{book_uid}', response_model=Book)
-async def update_book(book_uid: str, book_update_data: BookUpdateModel, session: AsyncSession = Depends(get_session), user_data=Depends(access_token_bearer), _: bool = Depends(role_checker)) -> dict:
+async def update_book(book_uid: str, book_update_data: BookUpdateModel, session: AsyncSession = Depends(get_session), token_details: dict=Depends(access_token_bearer), _: bool = Depends(role_checker)) -> dict:
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
     if updated_book is None:
         raise HTTPException(
@@ -56,7 +67,7 @@ async def update_book(book_uid: str, book_update_data: BookUpdateModel, session:
 
 
 @book_router.delete('/{book_uid}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid: str, session: AsyncSession = Depends(get_session), user_data=Depends(access_token_bearer), _: bool = Depends(role_checker)):
+async def delete_book(book_uid: str, session: AsyncSession = Depends(get_session), token_details: dict=Depends(access_token_bearer), _: bool = Depends(role_checker)):
     book_to_delete = await book_service.delete_book(book_uid, session)
     if book_to_delete is None:
         raise HTTPException(
