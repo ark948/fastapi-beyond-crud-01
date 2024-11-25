e8a204844ca71a1a186b6429c2dc0bbce3cd639f
ark948 <armankarimi948@gmail.com>
2024-11-25 22:43:27 +0330

    Refactoring on error handlers

--- src/errors.py
+++ src/errors.py
@@ -1,6 +1,8 @@
 from typing import Any, Callable
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi import status, FastAPI
+from sqlalchemy.exc import SQLAlchemyError
 
 
 
@@ -10,11 +12,9 @@ class BooklyException(Exception):
     pass
 
 
-
 class InvalidToken(BooklyException):
     """ User has provided an invalid or expired token """
     pass
-
 
 
 class RevokedToken(BooklyException):
@@ -22,11 +22,9 @@ class RevokedToken(BooklyException):
     pass
 
 
-
 class AccessTokenRequired(BooklyException):
     """ User has provided a refresh token when an access token is required """
     pass
-
 
 
 class RefreshTokenRequired(BooklyException):
@@ -34,11 +32,9 @@ class RefreshTokenRequired(BooklyException):
     pass
 
 
-
 class UserAlreadyExists(BooklyException):
     """ User has provided an email for a user who already exists during sign up """
     pass
-
 
 
 class InvalidCredentials(BooklyException):
@@ -46,11 +42,9 @@ class InvalidCredentials(BooklyException):
     pass
 
 
-
 class InsufficientPermission(BooklyException):
     """ User does not have neccessary permissions """
     pass
-
 
 
 class BookNotFound(BooklyException):
@@ -58,11 +52,9 @@ class BookNotFound(BooklyException):
     pass
 
 
-
 class TagNotFound(BooklyException):
     """ Tag not found """
     pass
-
 
 
 class TagAlreadyExists(BooklyException):
@@ -70,18 +62,14 @@ class TagAlreadyExists(BooklyException):
     pass
 
 
-
 class UserNotFound(BooklyException):
     """" User not found """
     pass
 
 
-
 class AccountNotVerified(BooklyException):
     """ User has not verified their account yet """
     pass
-
-
 
 
 # Exception handler
@@ -92,3 +80,165 @@ def create_exception_handler(status_code: int, initial_detail: Any) -> Callable[
             status_code=status_code
         )
     return exception_handler
+
+
+def register_all_errors(app: FastAPI):
+    app.add_exception_handler(
+        UserAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "User with email already exists",
+                "error_code": "user_exists",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        UserNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "User not found",
+                "error_code": "user_not_found",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "Book not found",
+                "error_code": "book_not_found",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InvalidCredentials,
+        create_exception_handler(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            initial_detail={
+                "message": "Invalid Email Or Password",
+                "error_code": "invalid_email_or_password",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InvalidToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid Or expired",
+                "resolution": "Please get new token",
+                "error_code": "invalid_token",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        RevokedToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid or has been revoked",
+                "resolution": "Please get new token",
+                "error_code": "token_revoked",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        AccessTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Please provide a valid access token",
+                "resolution": "Please get an access token",
+                "error_code": "access_token_required",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        RefreshTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Please provide a valid refresh token",
+                "resolution": "Please get an refresh token",
+                "error_code": "refresh_token_required",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InsufficientPermission,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "You do not have enough permissions to perform this action",
+                "error_code": "insufficient_permissions",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        TagNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={"message": "Tag Not Found",
+                            "error_code": "tag_not_found"},
+        ),
+    )
+
+    app.add_exception_handler(
+        TagAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Tag Already exists",
+                "error_code": "tag_exists",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "Book Not Found",
+                "error_code": "book_not_found",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        AccountNotVerified,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Account Not verified",
+                "error_code": "account_not_verified",
+                "resolution": "Please check your email for verification details"
+            },
+        ),
+    )
+
+    @app.exception_handler(500)
+    async def internal_server_error(request, exc):
+
+        return JSONResponse(
+            content={
+                "message": "Oops! Something went wrong",
+                "error_code": "server_error",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    @app.exception_handler(SQLAlchemyError)
+    async def database__error(request, exc):
+        print(str(exc))
+        return JSONResponse(
+            content={
+                "message": "Oops! Something went wrong",
+                "error_code": "server_error",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
