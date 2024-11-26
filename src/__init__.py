b073628efb3fc1d76447419ca04969de76203948
ark948 <armankarimi948@gmail.com>
2024-11-26 18:37:57 +0330

    middlewares

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from src.reviews.routes import review_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 from src.errors import register_all_errors
+from src.middleware import register_middleware
 
 
 @asynccontextmanager
@@ -25,6 +26,7 @@ app = FastAPI(
 
 
 register_all_errors(app)
+register_middleware(app)
 
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
